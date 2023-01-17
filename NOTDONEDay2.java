5ed1b2826394b101a9747d8f02d109bf6941d074
Ananda Zuraenee Sudarmaji <97554212+zuraenee@users.noreply.github.com>
2023-01-17 10:47:26 +0000

    Create NOTDONEDay2.java

---
+++ NOTDONEDay2.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.event.*;
+public class currencyConverter{
+  public static void main(String[] args){
+    Jframe frame = new Jframe("Currency Converter);
+    frame.setDefaultCloseOperation(Jframe.EXIT_ON_CLOSE);
+    Jbutton button1 = new Jbutton();
+    Jlabel title - new JLabel                          
+    frame.add(button1,BorderLayout.WEST);
+    frame.setSize(300,300);                          
+    frame.setVisible(true);
+    
+  }
+  public void actionPerformed(ActionEvent ae){
+    
+  }
+                         
+                            
+}
